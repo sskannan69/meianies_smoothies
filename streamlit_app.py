adf70f2d918853eb8e2ba71e46a0649fdadad9ed
sskannan69 <sskannan@hotmail.com>
2025-02-04 20:01:24 +0000

    Update streamlit_app.py
    import requests

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,6 +32,10 @@ if ingredients_list:
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
             values ('""" + ingredients_string + """','"""+name_on_order+ """')"""
 
+    import requests
+    smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+    st.text(smoothiefroot_response)
+
 
     time_to_insert  = st.button('Submit Order')
     
