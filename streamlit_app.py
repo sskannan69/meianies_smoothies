9a62fefa727909587946bfd046e90ab930bc70fe
sskannan69 <sskannan@hotmail.com>
2025-02-05 15:24:45 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,7 +34,7 @@ if ingredients_list:
         fv_dv = st.dataframe(data=fruityvice_response.json(),use_container_width=True)
 
         my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
-            values ('""" + ingredients_string + """','"""+name_on_order+ """')"""
+            values ('""" +ingredients_string+ """','"""+name_on_order+ """')"""
 
     time_to_insert  = st.button('Submit Order')
     
